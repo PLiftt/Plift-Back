81b09b62a3ecd5d9da6156d7e2402eab67826f44
danaevenegas <danaevenegasmaldonado@gmail.com>
2025-09-08 13:05:46 -0300

    modificar modelo user

--- authentication/models.py
+++ authentication/models.py
@@ -8,11 +8,22 @@ class User(AbstractUser):
     class Role(models.TextChoices):
         COACH = 'coach', 'Coach'
         ATHLETE = 'athlete', 'Athlete'
-    
+
+    class Gender(models.TextChoices):
+        MALE = 'male', 'Masculino'
+        FEMALE = 'female', 'Femenino'
+        OTHER = 'other', 'Otro'
+
+    second_name = models.CharField(max_length=150, blank=True, null=True)
+    second_last_name = models.CharField(max_length=150, blank=True, null=True)
+    gender = models.CharField(max_length=10, choices=Gender.choices, blank=True, null=True)
+    date_of_birth = models.DateField(blank=True, null=True)
+
     role = models.CharField(max_length=10, choices=Role.choices, default=Role.ATHLETE)
 
     def __str__(self):
-        return f"{self.username} ({self.role})"
+        full_name = f"{self.first_name} {self.second_name or ''} {self.last_name} {self.second_last_name or ''}".strip()
+        return f"{self.username} - {full_name} ({self.role}, {self.gender}, {self.date_of_birth})"
 
 # Agregar modelo invitation
 class Invitation(models.Model):
