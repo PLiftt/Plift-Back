9ae6b8834505b3908a1a44c5e5a036ffd63b6eca
danaevenegas <danaevenegasmaldonado@gmail.com>
2025-09-10 14:13:42 -0300

    correciones

--- back_plift/urls.py
+++ back_plift/urls.py
@@ -1,21 +1,17 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from authentication.views import RegisterView, UserViewSet
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView 
-from authentication.views import InvitationViewSet, CoachAthleteViewSet, ProfileView
+from authentication.urls import router as auth_router
+from authentication.urls import urlpatterns as auth_urls
 
 # Router para los ViewSets
 router = DefaultRouter()
-router.register(r'users', UserViewSet, basename='user')
-router.register(r'invitations', InvitationViewSet, basename='invitation')
-router.register(r'coachathletes', CoachAthleteViewSet, basename='coachathlete')
+router.registry.extend(auth_router.registry)
 
 urlpatterns = [
-    path("register/", RegisterView.as_view(), name="register"),
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
-    path("profile/", ProfileView.as_view(), name="profile"),
 
     
     path("", include(router.urls)),
-]
+] + auth_urls
